5fcf6430404c495d45ac1ae4cb90ce901ab16c3a
Danielle Hillman <hillm108@mail.chapman.edu>
2020-07-30 19:52:33 -0700

    added ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# workdayschedule
+
+# Work Day Schedule
+
+Created by me: *Danielle Hillman*
+Link to deployed application: https://daniellehillman.github.io/workdayschedule
+
+## The Daily Planner 
+Under the description of the scheduler, the current date is displayed.
+
+![](images/Top.png)
+
+This planner is broken down by hour from 9-5pm. For each hour, the user can enter in any content to add to their calendar. After pushing 'Add to Calendar', the content is stored in local storage and will remain on the page after being refreshed. 
+
+![](images/Content.png)
+
+
+## Color Blocking 
+
+Depending on the hour of the day, the color of each block will change. If the hour has past, the time block will be gray. If it is the current hour, the block will be orange. Lastly, hours in the future will be yellow. 
+
+
+![](images/Colors.png)
+
+
+
+## Future Updating & Additions
+Adding lists or rows into each hour time block could allow the user to enter in content in a helpful way. This could be an added tool to this application in the future.
--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 
-
+// displays the current time in the div with id currentDay 
+// uses moment 
 $('#currentDay').text(`
 ${moment().format('dddd, MMMM Do')}
 `)
 
-
+// changes the time from string to an integer
 let time = parseInt(moment().format('HH'));
-console.log(time)
 
-
+// funtion that loops for each hour 
 function timeBlock () {
     for (i=9; i <= 17; i++) {
+        // if the time has past, assigning past class to current id 
         if (i < time) {
             console.log(time)
             let block = $(`#${i}`).attr('id')
             $(`#${i}`).addClass('past')
             console.log(block)
+            // if the time is in the future, assigning future class to current id 
         } else if (i > time) {
             console.log("time")
             let block = $(`#${i}`).attr('id')
             $(`#${i}`).addClass('future')
             console.log(block)
+             // if the time is this hour, assigning present class to current id 
         } else {
             console.log(time)
             let block = $(`#${i}`).attr('id')
@@ -46,8 +49,12 @@ $('#17').val(localStorage.getItem('17'))
     $(document).ready(function() {
 
       $('.saveBtn').click(function() {
+        //   this is the value of the content in 'this' time block 
        let chore = ($(this).prev().val())
+    //    this takes the id from the button they push 
+    // assigning the id to the content 
        let place = ($(this).prev().attr('id'))
+    //    storing it in local storage 
        localStorage.setItem(place,chore)
 
       })})
